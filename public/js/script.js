f088ca92eb39efef3128af465d9d10ccf6df0276
AnggaExca <57145751+AnggaExca@users.noreply.github.com>
2020-05-20 23:09:56 +0700

    update v.2 - hafidh

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function () {
+	// scroll otomatis ke bagian tertentu sesuai dengan
+	// link yang diklik
 	var navLink = document.querySelectorAll(".nav-link");
 	navLink.forEach(function (link) {
 		link.addEventListener("click", function () {
@@ -8,13 +10,20 @@ window.addEventListener("load", function () {
 			}, 0);
 		});
 	});
+
 	window.addEventListener("scroll", function () {
+		// menampilkan navbar ketika posisi scroll tidak berada
+		// di tingkat paling atas
 		var nav = document.querySelector(".navbar");
 		if (window.pageYOffset > 0) {
-			nav.classList.add("nav-scroll");
+			//nav.classList.add("nav-scroll");
+			nav.style['background-color'] = 'black';
 		} else {
-			nav.classList.remove("nav-scroll");
+			//nav.classList.remove("nav-scroll");
+			nav.style['background-color'] = 'transparent';
 		}
+
+		// menampilkan ilustrasi ketika scroll
 		var illustration = document.querySelectorAll(".illustration");
 		illustration.forEach(function (pic) {
 			if (window.pageYOffset >= pic.offsetTop - window.innerHeight / 2) {
@@ -24,4 +33,23 @@ window.addEventListener("load", function () {
 			}
 		});
 	});
+
+	// untuk menampilkan dropdown pada prosedur
+	// pemesanan layanan
+	let dropItemTitles = document.querySelectorAll('.dd-item-title');
+	for (let i = 0; i < 4; i++) {
+		dropItemTitles[i].addEventListener('click', function() {
+			let content = this.nextElementSibling;
+			let ddActiveSign = 'plus-' + (i+1);
+			console.log(ddActiveSign);
+
+			if (content.style.maxHeight) {
+				content.style.maxHeight = null;
+				document.getElementById(ddActiveSign).innerHTML = '+';
+			} else {
+				content.style.maxHeight = content.scrollHeight + "px";
+				document.getElementById(ddActiveSign).innerHTML = '-';
+			}
+		});
+	}
 });
